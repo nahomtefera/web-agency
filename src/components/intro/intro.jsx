8932b23baf4c726aa6da06b4bc09862b3bba32d3
nahom <nahumtefera@gmail.com>
2018-04-19 18:33:16 -0700

    added new logo and intro image

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -6,11 +6,13 @@ class Intro extends Component {
     render() {
         return (
             <div className="intro-container">
+                <img className="intro-logo" src={require('../../images/logo.png')} alt="logo"/>
                 <div className="intro-text-container">
-                    <h3 className="intro-text">It's time to get the best out of your business</h3>
+                    <h3 className="intro-title">We craft beautiful websites</h3>
+                    <h3 className="intro-text">It's time to get the best out of your restaurant, give your website a new look and start atracting customers</h3>
                 </div>
                 <div className="intro-img-container">
-                    <img className="intro-img" src={require("../../images/intro-img.png")} alt="intro"/>
+                    <img className="intro-img" src={require("../../images/intro-img-mockup.png")} alt="intro"/>
                 </div>
             </div>
         )

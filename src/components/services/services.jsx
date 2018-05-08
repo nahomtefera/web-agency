1470311ed09c1868bb13211fccca461633d8972d
nahom <nahumtefera@gmail.com>
2018-05-08 15:38:56 -0700

    added new icons for Services component, added box-like style to services component, gave styes to services component

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -11,6 +11,26 @@ class Services extends Component {
         return (
             <div className="services-container">
                 <div className="services-title">Services</div>
+
+                <div className="services-boxes-container">
+                    <div className="services-boxes">
+                        <h4 className="services-boxes-title">Web Design</h4>
+                        <img className="services-img-logo" src={require('../../images/services-web-design.png')} alt=""/>
+                    </div>
+
+                    <div className="services-boxes">
+                        <h4 className="services-boxes-title">Social Media</h4>
+                        <img className="services-img-logo" src={require('../../images/services-social-media.png')} alt=""/>
+                        {/* <img className="services-img-logo services-social-media-icon" src={require('../../images/services-instagram.png')} alt=""/> */}
+
+                    </div>
+
+                    <div className="services-boxes">
+                        <h4 className="services-boxes-title">Analitycs</h4>
+                        <img className="services-img-logo" src={require('../../images/services-analitycs.png')} alt=""/>
+
+                    </div>
+                </div>
             </div>
         )
     }
